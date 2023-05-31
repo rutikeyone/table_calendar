1e3da34a49b57bc7a1d2cfdd3e05251bf2d0d46c
rutikeyone <rutikeyone@gmail.com>
2023-05-31 14:51:11 +0700

    Modified table_calendar

--- example/lib/pages/basics_example.dart
+++ example/lib/pages/basics_example.dart
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 import '../utils.dart';
@@ -16,6 +17,74 @@ class _TableBasicsExampleState extends State<TableBasicsExample> {
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
 
+  Widget _onCalendarHeaderBuilder(
+      locale,
+      DateTime focusedMonth,
+      CalendarFormat calendarFormat,
+      HeaderStyle headerStyle,
+      VoidCallback onLeftChevronTap,
+      VoidCallback onRightChevronTap,
+      VoidCallback onHeaderTap,
+      VoidCallback onHeaderLongPress,
+      ValueChanged<CalendarFormat> onFormatButtonTap,
+      Map<CalendarFormat, String> availableCalendarFormats,
+      DayBuilder? headerTitleBuilder) {
+    final text = headerStyle.titleTextFormatter?.call(focusedMonth, locale) ??
+        DateFormat.yMMMM(locale).format(focusedMonth);
+    return Container(
+      decoration: headerStyle.decoration,
+      margin: headerStyle.headerMargin,
+      padding: headerStyle.headerPadding,
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          if (headerStyle.leftChevronVisible)
+            CustomIconButton(
+              icon: headerStyle.leftChevronIcon,
+              onTap: onLeftChevronTap,
+              margin: headerStyle.leftChevronMargin,
+              padding: headerStyle.leftChevronPadding,
+            ),
+          Expanded(
+            child: headerTitleBuilder?.call(context, focusedMonth) ??
+                GestureDetector(
+                  onTap: onHeaderTap,
+                  onLongPress: onHeaderLongPress,
+                  child: Text(
+                    text,
+                    style: headerStyle.titleTextStyle,
+                    textAlign: headerStyle.titleCentered
+                        ? TextAlign.center
+                        : TextAlign.start,
+                  ),
+                ),
+          ),
+          if (headerStyle.formatButtonVisible &&
+              availableCalendarFormats.length > 1)
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: FormatButton(
+                onTap: onFormatButtonTap,
+                availableCalendarFormats: availableCalendarFormats,
+                calendarFormat: calendarFormat,
+                decoration: headerStyle.formatButtonDecoration,
+                padding: headerStyle.formatButtonPadding,
+                textStyle: headerStyle.formatButtonTextStyle,
+                showsNextFormat: headerStyle.formatButtonShowsNext,
+              ),
+            ),
+          if (headerStyle.rightChevronVisible)
+            CustomIconButton(
+              icon: headerStyle.rightChevronIcon,
+              onTap: onRightChevronTap,
+              margin: headerStyle.rightChevronMargin,
+              padding: headerStyle.rightChevronPadding,
+            ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,11 +92,13 @@ class _TableBasicsExampleState extends State<TableBasicsExample> {
         title: Text('TableCalendar - Basics'),
       ),
       body: TableCalendar(
+        calendarHeaderBuilder: _onCalendarHeaderBuilder,
         calendarStyle: CalendarStyle(
-            todayDecoration: const BoxDecoration(
-              shape: BoxShape.circle,
-            ),
-            todayTextStyle: TextStyle(color: Colors.black),),
+          todayDecoration: const BoxDecoration(
+            shape: BoxShape.circle,
+          ),
+          todayTextStyle: TextStyle(color: Colors.black),
+        ),
         headerStyle:
             HeaderStyle(formatButtonVisible: false, titleCentered: true),
         firstDay: kFirstDay,

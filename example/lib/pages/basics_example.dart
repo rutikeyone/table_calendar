ca35b16e12cf4e0113b2b594b090895457e185c0
rutikeyone <rutikeyone@gmail.com>
2023-05-31 11:15:56 +0700

    Change intl version

--- example/lib/pages/basics_example.dart
+++ example/lib/pages/basics_example.dart
@@ -23,21 +23,22 @@ class _TableBasicsExampleState extends State<TableBasicsExample> {
         title: Text('TableCalendar - Basics'),
       ),
       body: TableCalendar(
+        calendarStyle: CalendarStyle(
+            todayDecoration: const BoxDecoration(
+              shape: BoxShape.circle,
+            ),
+            todayTextStyle: TextStyle(color: Colors.black),),
+        headerStyle:
+            HeaderStyle(formatButtonVisible: false, titleCentered: true),
         firstDay: kFirstDay,
         lastDay: kLastDay,
         focusedDay: _focusedDay,
         calendarFormat: _calendarFormat,
         selectedDayPredicate: (day) {
-          // Use `selectedDayPredicate` to determine which day is currently selected.
-          // If this returns true, then `day` will be marked as selected.
-
-          // Using `isSameDay` is recommended to disregard
-          // the time-part of compared DateTime objects.
           return isSameDay(_selectedDay, day);
         },
         onDaySelected: (selectedDay, focusedDay) {
           if (!isSameDay(_selectedDay, selectedDay)) {
-            // Call `setState()` when updating the selected day
             setState(() {
               _selectedDay = selectedDay;
               _focusedDay = focusedDay;
@@ -46,14 +47,12 @@ class _TableBasicsExampleState extends State<TableBasicsExample> {
         },
         onFormatChanged: (format) {
           if (_calendarFormat != format) {
-            // Call `setState()` when updating calendar format
             setState(() {
               _calendarFormat = format;
             });
           }
         },
         onPageChanged: (focusedDay) {
-          // No need to call `setState()` here
           _focusedDay = focusedDay;
         },
       ),
